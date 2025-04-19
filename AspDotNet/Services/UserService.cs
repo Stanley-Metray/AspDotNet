d97ca2bfe1996b527d0dd05f3be25c500c8de7c5
MacBook <stanleymetray@gmail.com>
2025-04-19 17:00:08 +0530

    Usage of Nested Model

---
+++ AspDotNet/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AspDotNet.Models;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace AspDotNet.Services
+{
+	public class UserService
+	{
+		private readonly HttpClient _httpClient;
+
+		public UserService(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+        public async Task<List<NestedUserModel>> GetUsersAsync()
+        {
+			try
+			{
+                var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+
+				if (response!=null && response.IsSuccessStatusCode)
+				{
+					var json = await response.Content.ReadAsStringAsync();
+					var users = JsonConvert.DeserializeObject<List<NestedUserModel>>(json);
+					return users;
+				}
+            }
+            catch(Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+            return new List<NestedUserModel>();
+        }
+    }
+
+}
+
