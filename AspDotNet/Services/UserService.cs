7171b4d0f144b429a377f8192ef9fd6a82100af3
MacBook <stanleymetray@gmail.com>
2025-04-20 14:30:55 +0530

    Implemented Text Cache or In-Memory Cache

--- AspDotNet/Services/UserService.cs
+++ AspDotNet/Services/UserService.cs
@@ -2,20 +2,31 @@
 using AspDotNet.Models;
 using System.Text;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace AspDotNet.Services
 {
 	public class UserService
 	{
 		private readonly HttpClient _httpClient;
+		private readonly IMemoryCache _cache;
 
-		public UserService(HttpClient httpClient)
+		public UserService(HttpClient httpClient, IMemoryCache cache)
 		{
 			_httpClient = httpClient;
+			_cache = cache;
 		}
 
         public async Task<List<NestedUserModel>> GetUsersAsync()
         {
+			const string cacheKey = "userList";
+
+			if(_cache.TryGetValue(cacheKey, out List<NestedUserModel> cachedUsers))
+			{
+				Console.WriteLine("Returning Users From Cache");
+				return cachedUsers;
+			}
+
 			try
 			{
                 var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
@@ -24,6 +35,12 @@ namespace AspDotNet.Services
 				{
 					var json = await response.Content.ReadAsStringAsync();
 					var users = JsonConvert.DeserializeObject<List<NestedUserModel>>(json);
+
+					// set users into cache
+
+					_cache.Set(cacheKey, users, TimeSpan.FromMinutes(1));
+
+					Console.WriteLine("Returning Users From API");
 					return users;
 				}
             }
