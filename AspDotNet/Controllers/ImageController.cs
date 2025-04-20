6499cb740f72fafa877572b4a2fbcffcf4e20c51
MacBook <stanleymetray@gmail.com>
2025-04-20 19:22:36 +0530

    Image Cached Functionality URL and Disk Cached

---
+++ AspDotNet/Controllers/ImageController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspDotNet.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspDotNet.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly ImageService _imageService;
+
+        public ImageController(ImageService imageService)
+        {
+            _imageService = imageService;
+        }
+        
+        public IActionResult CacheImageUrl()
+        {
+            string imageUrl = "https://picsum.photos/400/300?random=1";
+            string cachedUrl = _imageService.GetCachedImageUrl(imageUrl);
+
+            ViewBag.ImageUrl = cachedUrl;
+            return View();
+        }
+
+        public async Task<IActionResult> CacheImage()
+        {
+            string imageUrl = "https://picsum.photos/400/300?random=2";
+            string localPath = await _imageService.GetCachedImagePathAsync(imageUrl);
+
+            string fileName = Path.GetFileName(localPath);
+            ViewBag.ImagePath = $"/cachedImages/{fileName}";
+            return View();
+        }
+    }
+}
+
